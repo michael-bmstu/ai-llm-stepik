ae7ad4e7cb56ea8fb5f6655760ce7a7bf8f9da63
Mihail Solovev <solovei.0807@gmail.com>
2026-01-05 21:24:44 +0300

    add chainlit folder

---
+++ 07_chainlit/01_chatbot.py
@@ -0,0 +1,36 @@
+import chainlit as cl
+from langchain_core.runnables import RunnableConfig
+from langchain_core.runnables.history import RunnableWithMessageHistory, BaseChatMessageHistory
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chat_models import init_chat_model
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DOMAIN = "biology"
+session_store = dict()
+
+def get_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in session_store:
+        session_store[session_id] = InMemoryChatMessageHistory()
+    return session_store[session_id]
+
+messages = [
+    ("system", "You are an expert in {domain}. Your task is answer the question as short as possible"),
+    ("placholder", "{history}"),
+    ("human", "{question}"),
+]
+
+prompt = ChatPromptTemplate(messages)
+llm = init_chat_model(model="mistral-large-latest", api_key=os.getenv("MISTRAL_KEY"))
+chain = RunnableWithMessageHistory(
+   prompt | llm, get_session_history=get_history,
+   input_messages_key="question", history_messages_key="history",
+) | StrOutputParser()
+
+@cl.on_message
+async def handle_message(message: cl.Message):
+    pass
---
+++ 07_chainlit/README.md
@@ -0,0 +1,10 @@
+# UI for LLM Apps
+This folder contains examples UI for Chat-bot systems
+
+1. Simple chat-bot with:
+    - user sessions
+    - chat profile
+    - app settings
+    - actions (feedback)
+2. RAG app
+    - ...
