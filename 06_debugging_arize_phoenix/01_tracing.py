06d439c6d5e48c06d528c07e60c2606b8c461a73
Mihail Solovev <solovei.0807@gmail.com>
2026-01-05 11:11:02 +0300

    fixed runtime errors

--- 06_debugging_arize_phoenix/01_tracing.py
+++ 06_debugging_arize_phoenix/01_tracing.py
@@ -8,7 +8,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_mistralai import MistralAIEmbeddings
-from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_huggingface import HuggingFaceEmbeddings
 # chain
 from langchain_core.runnables import RunnableLambda, RunnableParallel, RunnableConfig
 # trace
@@ -22,11 +22,11 @@ import time
 
 load_dotenv()
 
-tracer = register(project_name="trace-example", endpoint="https://localhost:6006/v1/traces")
+tracer = register(project_name="trace-example", endpoint="http://localhost:6006/v1/traces")
 LangChainInstrumentor().instrument(tracer_provider=tracer)
 
 loader = PyPDFLoader("06_debugging_arize_phoenix\paper.pdf")
-pages = loader.load()[:10]
+pages = loader.load()[:5]
 full_text = "\n".join(page.page_content for page in pages)
 splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=100, add_start_index=True)
 chunks = splitter.split_text(full_text)
@@ -36,8 +36,7 @@ docs = [Document(page_content=chunk, metadata={"source": "paper.pdf"}) for chunk
 embeddings = MistralAIEmbeddings(model="mistral-embed", api_key=os.getenv("MISTRAL_KEY"))
 
 vectorstore = InMemoryVectorStore.from_documents(docs, embedding=embeddings)
-retriver = vectorstore.as_retriever(k=3)
-time.sleep(2)
+retriver = vectorstore.as_retriever(search_kwargs={'k': 3})
 
 llm = init_chat_model(model="mistral-medium-latest", api_key=os.getenv("MISTRAL_KEY"), temperature=0)
 prompt = ChatPromptTemplate([
@@ -49,7 +48,7 @@ prompt = ChatPromptTemplate([
     ("human", "Question: {question}")
 ])
 
-format_context = RunnableLambda(lambda docs: "\n\n".join(d.page_contetn for d in docs))\
+format_context = RunnableLambda(lambda docs: "\n\n".join(d.page_content for d in docs))\
     .with_config(config=RunnableConfig(run_name="format documents"))
 
 chain = RunnableParallel(context=retriver | format_context, question=lambda d: d) | prompt | llm | StrOutputParser()
