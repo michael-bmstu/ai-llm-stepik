d84f7a45ab1350d57af41b86c2eee687a2c9deef
Mihail Solovev <solovei.0807@gmail.com>
2026-01-06 19:55:37 +0300

    docs: add comments

--- 07_chainlit/02_rag_ui.py
+++ 07_chainlit/02_rag_ui.py
@@ -28,16 +28,15 @@ prompt = ChatPromptTemplate([
     ("human", "{question}")
 ])
 
+# Model init
 llm = init_chat_model(model="mistral-large-latest", api_key=API_KEY)
 embeddings = init_embeddings(model="mistralai:mistral-embed", api_key=API_KEY)
-
-
 
 @cl.on_chat_start
 async def add_data_base():
     files = None
     while not files:
-        user_file = cl.AskFileMessage(
+        user_file = cl.AskFileMessage( # get file from user in dialogue
             content="Please, upload the file",
             accept=["application/pdf"],
             max_size_mb=20,
@@ -45,9 +44,10 @@ async def add_data_base():
         )
         files = await user_file.send()
         if not files:
-            await user_file.remove()
+            await user_file.remove() # keep waiting
 
     file = files[0]
+    # create info message
     process_msg = cl.Message(content=f"Processing file {file.name}")
     await process_msg.send()
 
@@ -57,45 +57,49 @@ async def add_data_base():
     await process_msg.update()
     
     vectorstore = InMemoryVectorStore(embeddings)
+    # get timeout when processing many chunks to avoid blocking free mistralai api
     async def add_documents_with_timeout(docs, timeout:float=2, batch:int=16):
-        for i in range(0, len(docs), batch):
+        for i in range(0, len(docs), batch): # processing by batch
             cur_timeout = timeout
             while True:
                 try:
                     vectorstore.add_documents(docs[i:i + batch])
                     break
                 except:
-                    cur_timeout *= 3
+                    cur_timeout *= 3 # increace timeout to fix error
                     time.sleep(cur_timeout)
             time.sleep(cur_timeout)
             process_msg.content = f"Processed {min(i + batch, len(docs))} chunks"
             await process_msg.update()
+        
     await add_documents_with_timeout(chunks)
     process_msg.content = f"Vector store from chunks was created"
     await process_msg.update()
 
-
     retriver = vectorstore.as_retriever(search_kwargs={"k": 3})
+    # Concat retrived files
     format_context = RunnableLambda(lambda docs: "\n\n".join(d.page_content for d in docs))\
     .with_config(config=RunnableConfig(run_name="format documents"))
-
+    # chain with retrive, concat, answer 
     chain = RunnableParallel(context=retriver | format_context, question=lambda q: q
         ) | RunnableParallel(
             answer=prompt | llm | StrOutputParser(), chunks=lambda d: d["context"], prompt=prompt
         )
     cl.user_session.set("chain", chain)
-    cl.sleep(2)
+    cl.sleep(2) # avoid blocking free mistralai api
     process_msg.content = "Processing is complete, the chatbot is ready to work"
     await process_msg.update()
 
 @cl.on_message
 async def main(message: cl.Message):
-    chain: Runnable = cl.user_session.get("chain")
+    chain: Runnable = cl.user_session.get("chain") # get chain with database for current user
     res = await chain.ainvoke(message.content)
+    # add context documents to answer to improve interpretability
     context_chunks = [cl.Text(content=chunk, name=f"Context fragment {i}", display="inline")
                        for i, chunk in enumerate(res["chunks"].split("\n\n"))]
     await cl.Message(content=res["answer"], elements=context_chunks).send()
 
 """
+run command
 chainlit run 07_chainlit/02_rag_ui.py
 """
