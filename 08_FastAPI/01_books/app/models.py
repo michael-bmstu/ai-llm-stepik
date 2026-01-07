3c81c4db8b23365a26aa20d0fb6ceaccbbf337ea
Mihail Solovev <solovei.0807@gmail.com>
2026-01-07 09:10:45 +0300

    add models

---
+++ 08_FastAPI/01_books/app/models.py
@@ -0,0 +1,21 @@
+from uuid import uuid4, UUID
+from pydantic import BaseModel, Field
+
+
+class BaseBook(BaseModel):
+    name: str = Field(examples=["Дубровский"])
+    author: str = Field(examples=["А. С. Пушкин"])
+    year: str = Field(examples=["1843"])
+    annotation: str = Field(examples=["Классический роман о любви и чести"])
+
+class Book(BaseBook):
+    id: UUID = Field(examples=[uuid4()])
+
+class CreateBook(BaseBook):
+    pass
+
+class UpdateBook(BaseBook):
+    pass
+
+class SucccessMessage(BaseModel):
+    message: str = Field(examples=["Операция выполнена"])
