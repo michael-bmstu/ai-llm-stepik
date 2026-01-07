5f5046bd338b68d55f868a4537a4017da572ec60
Mihail Solovev <solovei.0807@gmail.com>
2026-01-07 13:18:14 +0300

    run docker container

--- 09_docker/01_fastapi.py
+++ 09_docker/01_fastapi.py
@@ -7,9 +7,14 @@ def home():
     return {"message": "Hello, world!"}
 
 
-"""
+"""set venv
 uv venv
 uv init --no-workspace --bare
 uv add fastapi fastapi-cli
 uv run fastapi 01_fastapi.py --host 0.0.0.0
+"""
+
+"""docker
+docker build -t fastapi-uv .
+docker run -p 8000:8000 fastapi-uv
 """
