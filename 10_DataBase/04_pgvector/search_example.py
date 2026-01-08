58015c10393b84e0ced8e8f4bc76cedf439b1a8b
Mihail Solovev <solovei.0807@gmail.com>
2026-01-08 18:25:10 +0300

    add vector search example

--- 10_DataBase/04_pgvector/search_example.py
+++ 10_DataBase/04_pgvector/search_example.py
@@ -19,4 +19,18 @@ LIMIT $2;
 """
 
 embeddings = init_embeddings(model="mistralai:mistral-embed", api_key=os.getenv("MISTRAL_KEY"))
-query_vector = embeddings.embed_query("Что такое большая языковая модель?")
+query_text = "Что такое большая языковая модель?"
+
+async def get_relevant(text: str, limit: int=3):
+    query_vector = await embeddings.aembed_query(text)
+    conn: asyncpg.Connection = await asyncpg.connect(DATABASE_DSN)
+    try:
+        await register_vector(conn)
+        res = await conn.fetch(GET_NEAREST_DATA_SQL, query_vector, limit)
+        pprint(res)
+    finally:
+        if conn:
+            await conn.close()
+
+
+asyncio.run(get_relevant(query_text, 2))
