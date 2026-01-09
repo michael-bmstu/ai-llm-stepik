23e808871e8789ce7006033a7dd371244bffcc0e
Mihail Solovev <solovei.0807@gmail.com>
2026-01-09 13:59:07 +0300

    feat: add qdarnt example (init vector db)

---
+++ 10_DataBase/05_qdrant/init_db.py
@@ -0,0 +1,49 @@
+import uuid
+from qdrant_client import QdrantClient
+from qdrant_client.http.exceptions import UnexpectedResponse
+from qdrant_client.http.models import Distance, VectorParams
+from langchain.embeddings import init_embeddings
+from langchain_qdrant import QdrantVectorStore
+from langchain_core.documents import Document
+import pandas as pd
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+COLLECTION_NAME = "spams"
+DATASET_PATH = "10_DataBase/05_qdrant/spams.csv"
+
+df = pd.read_csv(DATASET_PATH)
+
+embeddings = init_embeddings(model="mistralai:mistral-embed", api_key=os.getenv("MISTRAL_KEY"))
+texts = df['Message'].to_list()
+spam_facts = df["Category"].map(lambda cat: cat == "spam").to_list()
+docs = [Document(page_content=text, metadata={"is_spam": spam}) for text, spam in zip(texts, spam_facts)]
+
+client = QdrantClient(url="http://localhost:6333")
+try:
+    client.create_collection(COLLECTION_NAME, vectors_config=VectorParams(size=1024, distance=Distance.COSINE),)
+except UnexpectedResponse:
+    client.delete_collection(COLLECTION_NAME)
+    client.create_collection(COLLECTION_NAME, vectors_config=VectorParams(size=1024, distance=Distance.COSINE),)
+
+qdrant = QdrantVectorStore(client, COLLECTION_NAME, embeddings)
+print("Collections created")
+
+batch = 16
+timeout = 1.5
+for i in range(0, len(docs), batch): # processing by batch
+    cur_timeout = timeout
+    while True:
+        try:
+            qdrant.add_documents(docs[i:i + batch])
+            break
+        except:
+            cur_timeout *= 3 # increace timeout to fix error
+            time.sleep(cur_timeout)
+    time.sleep(cur_timeout)
+    print("processed", min(i + batch, len(docs)), "samples")
+
+print("Data was vectorized")
